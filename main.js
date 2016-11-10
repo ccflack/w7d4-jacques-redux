9cb157c4ba682ca3c895776712155b600dc5f6d1
ccflack <ccflack@me.com>
2016-11-10 17:40:14 -0500

    Notes, tags displaying. Form is present, not functional.

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-$.getJSON('https://git.heroku.com/desolate-badlands-26376.git')
+function clear_form(selector){
+  $(selector)[0].reset();
+  toggle_submit(selector)
+}
+
+var notes_source   = $("#notes-template").html()
+var note_template = Handlebars.compile(notes_source)
+
+$.getJSON('https://desolate-badlands-26376.herokuapp.com/api/notes')
 .then(function(response){
-  return response
+  return response.notes.forEach(function(note){
+    var display_notes = note_template(note)
+    $('#notes').prepend(display_notes)
+  })
+})
+
+$('#note_form').on('submit', function(ev){
+  ev.preventDefault()
+  $.post('https://desolate-badlands-26376.herokuapp.com/api/notes',
+    $(this).serializeArray()
+  ).done(function(response){
+    handle_user_forms(response, '#note_form')
+  }).fail(function(response){
+    handle_errors('#note_form', response)
+  })
 })
